5219ea1c6107e43c2ea847316d6d65af66a81f30
Dario Fernandez <dariofedes@protonmail.com>
2024-03-03 12:43:31 +0100

    remove getters from Product

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -5,17 +5,8 @@ export default class Product {
     this.image = productDTO.image
     this.status = productDTO.status
     this.location = productDTO.location.name
-  }
-
-  get price() {
-    return this.id * 100
-  }
-
-  get hasStock() {
-    return this.status === 'Alive'
-  }
-
-  get hasKnownLocation() {
-    return this.location !== 'unknown'
+    this.price =  this.id * 100
+    this.hasStock = this.status === 'Alive'
+    this.hasKnownLocation = this.location !== 'unknown'
   }
 }
